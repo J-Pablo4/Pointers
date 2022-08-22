ee37613cd9bbb063b0a9cb5af2ecb647f20f2345
Juan Pablo <pablo.lopez@iteso.mx>
2022-08-22 10:24:09 -0500

    Apuntador de arreglos_1

--- main.c
+++ main.c
@@ -2,11 +2,13 @@
 
 int main(void)
 {
-    char una_letra = 'z';
-    printf("%lu\n", sizeof(una_letra));
+    int nombre_del_arreglo[10] = {4,5,6,7};
 
-    int *un_ptr_de_entero = &una_letra;
+    printf("%d\n", nombre_del_arreglo[0]);
 
-    printf("%d\n", *un_ptr_de_entero);
+    int *mi_apuntador = &nombre_del_arreglo[0];
+
+    printf("%d", *mi_apuntador);
+
     return 0;
 }
