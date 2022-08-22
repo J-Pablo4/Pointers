cd7d8594b8a5f4733a3b95febfdec4e077498c0c
Juan Pablo <pablo.lopez@iteso.mx>
2022-08-22 09:57:27 -0500

    Inicio de apuntadores

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
+int main(void) {
+    int foo = 13;
+    char bar = 'a'; // Tamaño: de 1 byte hasta 4 bytes
+    printf("%d", foo);
+    printf("%d", sizeof(foo));
     return 0;
 }
